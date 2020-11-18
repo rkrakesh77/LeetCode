4b0eb4468f5aaa0d67b77d37723aca85c5847473
Rakesh Kumar <rkrakeshkumar771996@gmail.com>
2020-11-18 11:50:49 +0530

    Valid Parenthesis

---
+++ src/techrakesh/easyproblem/ValidParenthesis.java
@@ -0,0 +1,29 @@
+package techrakesh.easyproblem;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        System.out.println(isValid("(((())))"));  //TRUE
+        System.out.println(isValid("(((()))"));   //FALSE
+        System.out.println(isValid("({)}"));   //FALSE
+        System.out.println(isValid("({}[])"));   //TRUE
+    }
+    public static boolean isValid(String s) {
+        HashMap<Character,Character> maps=new HashMap<Character,Character>();
+        maps.put(')','(');
+        maps.put(']','[');
+        maps.put('}','{');
+        Stack<Character> stack=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(maps.containsKey(c)){
+                if(stack.empty()||stack.pop()!=maps.get(c))return false;
+            }
+            else
+                stack.push(c);
+        }
+        return stack.empty();
+    }
+}
