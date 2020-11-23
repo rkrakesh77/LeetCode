57f83ff10cb74aeb163088f7b354f33dc0bc2289
Rakesh Kumar <rkrakeshkumar771996@gmail.com>
2020-11-23 13:02:23 +0530

    Test

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println((int)('9'-'0'));
         System.out.println((int)'0');
+        System.out.println("Hello World");
     }
 }
